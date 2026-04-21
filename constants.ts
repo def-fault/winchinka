20b21257f1008009840ada1d60b29f5e2e385f0a
def-fault-self <def.fault.self@gmail.com>
2026-04-21 22:16:02 +0900

    result

--- constants.ts
+++ constants.ts
@@ -324,8 +324,8 @@ export const season3Participants = [
     ],
   },
   {
-    name: '이름이뭐에요전화버논뭐에요',
-    state: '저격 진행중',
+    name: '엄머머~',
+    state: '저격 불가',
     members: [
       { name: '비공개', class: '비공개' },
       { name: '비공개', class: '비공개' },
@@ -444,7 +444,6 @@ export const TOURNAMENTS: Tournament[] = [
     participants: season3Participants,
     preliminaryMatches: [
       { defenseTeam: '세얼간이', attackTeam: '청설모' },
-      { defenseTeam: '이름이뭐에요전화버논뭐에요', attackTeam: '엄머머~' },
       { defenseTeam: '어우사', attackTeam: '심판을 받아라' },
       { defenseTeam: 'LA 최고의 갱단', attackTeam: '미워도 다시 한번' },
     ],
