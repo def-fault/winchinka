7e5e1af72ce4aaba0653c8e7dacb0e504d588b08
def-fault-self <def.fault.self@gmail.com>
2026-04-23 22:05:24 +0900

    round

--- constants.ts
+++ constants.ts
@@ -379,7 +379,7 @@ export const season3Participants = [
   },
   {
     name: '왕밤빵짱',
-    state: '저격 가능',
+    state: '저격 진행중',
     members: [
       { name: '비공개', class: '비공개' },
       { name: '비공개', class: '비공개' },
@@ -449,6 +449,9 @@ export const TOURNAMENTS: Tournament[] = [
       { defenseTeam: '어우사', attackTeam: '심판을 받아라', winner: 'defense' },
       { defenseTeam: '갱주들', attackTeam: '광탈팀' },
       { defenseTeam: '꽃이필무렵', attackTeam: '이름없음' },
+      { defenseTeam: '왕밤빵짱', attackTeam: '롤러터' },
+      { defenseTeam: '미정', attackTeam: '올리브영' },
+      { defenseTeam: '미정', attackTeam: 'Ssiyo' },
     ],
     sponsors: [
       { name: "우위 (올리브영)" },
